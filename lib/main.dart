e6c3eca627eb4220ac3b0401cc13a8056931186d
apple <1234>
2024-10-31 10:10:58 +0400

    "Commit for real estate mobile app"

--- lib/main.dart
+++ lib/main.dart
@@ -34,19 +34,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(

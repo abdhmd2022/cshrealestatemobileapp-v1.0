c9194ff31926f4549eb4b1392f897be3b16b44d9
apple <1234>
2024-11-05 17:53:43 +0400

    "Commit for real estate mobile app"

---
+++ lib/CreateInquiry.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class CreateInquiry extends StatefulWidget {
+
+  @override
+  State<CreateInquiry> createState() => _CreateInquiryPageState();
+}
+
+class _CreateInquiryPageState extends State<CreateInquiry> {
+
+  @override
+  void initState() {
+    super.initState();
+
+
+    _initSharedPreferences();
+  }
+
+  Future<void> _initSharedPreferences() async {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(app_name,
+            style: TextStyle(
+                color: Colors.white
+            ),),
+        ),
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+
+            child:    Column(
+                children: [
+
+
+                ]))
+    );}}

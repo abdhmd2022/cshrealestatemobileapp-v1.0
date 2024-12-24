b289750310b785c3c43716e26c5d40a2acd20edf
apple <1234>
2024-12-24 12:36:05 +0400

    "Commit for real estate mobile app"

--- lib/SalesInquiryTransfer.dart
+++ lib/SalesInquiryTransfer.dart
@@ -1,13 +1,9 @@
-import 'dart:io';
 import 'package:cshrealestatemobile/constants.dart';
-import 'package:flutter/animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:image_picker/image_picker.dart';
-
 import 'SalesInquiryReport.dart';
 import 'Sidebar.dart';
 

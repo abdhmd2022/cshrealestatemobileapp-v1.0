382276e02ca0bbd8865f9a1e8bf0c1d934adbd13
apple <1234>
2025-01-20 09:57:36 +0400

    "Commit for real estate mobile app v1.0"

--- lib/constants.dart
+++ lib/constants.dart
@@ -12,10 +12,11 @@ const String authTokenBase = r'!1--3*%*%*%*9$api$8*%*%*%*5--0!X19fIUBBUyQlYXMxOT
 
 const String BASE_URL_config = "http://192.168.2.157:4000/api";
 
-const String Serial_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJpYXQiOjE3MzcxMDQ1NTMsImV4cCI6MTczNzE5MDk1M30.wJcH5QDjMI2Tm1rq-Pg1B8QR3RvgWy-my2ONzBaQtcs';
+const String Serial_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJpYXQiOjE3MzczNTEzNTYsImV4cCI6MTczNzQzNzc1Nn0.Hwq-A4_J6t240cwa2HR7teN5ermPYN16Xhv_yJN8UJU';
 
-const String Company_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJjb21wYW55X2lkIjoxLCJpYXQiOjE3MzcxMDQ1NTMsImV4cCI6MTczNzE5MDk1M30.EutVLraWu4Gebl7HuXdcjXems3PN23EDaDV8XfSA4Z0';
+const String Company_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJjb21wYW55X2lkIjoxLCJpYXQiOjE3MzczNTEzNTYsImV4cCI6MTczNzQzNzc1Nn0.MThvFbuJFet9S4p2607XGxoHHzhhQheZSsLilohILlU';
 
 
 /*
-final String uuid = "6e35f08d-8285-45e3-ae32-a0e9efe5407d";*/
+final String uuid = "6e35f08d-8285-45e3-ae32-a0e9efe5407d";
+*/

f85832b68cafd8fad5c1ae377381623aadd50bb0
apple <1234>
2025-01-23 11:43:05 +0400

    "Commit for real estate mobile app v1.0"

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 const String app_name = "Real Estate";
@@ -12,9 +10,9 @@ const String authTokenBase = r'!1--3*%*%*%*9$api$8*%*%*%*5--0!X19fIUBBUyQlYXMxOT
 
 const String BASE_URL_config = "http://192.168.2.157:4000/api";
 
-const String Serial_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJpYXQiOjE3Mzc1MjQ3OTcsImV4cCI6MTczNzYxMTE5N30.yUyuB4xwbI5dFf4971yLpF6Un8H37ZaDsV4AxLFV3Ag';
+const String Serial_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJpYXQiOjE3Mzc2MDkyNDcsImV4cCI6MTczNzY5NTY0N30.0Wir2ByqCdD9OiCkWuntb04al8U5pJ1Nlpn_skHsJXM';
 
-const String Company_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJjb21wYW55X2lkIjoxLCJpYXQiOjE3Mzc1MjQ3OTcsImV4cCI6MTczNzYxMTE5N30.m0UMNMIHB_zAdDuxSTdFWCOCyV9E_9A1yiLR2YY_b6g';
+const String Company_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic2VyaWFsX2lkIjoxLCJjb21wYW55X2lkIjoxLCJpYXQiOjE3Mzc2MDkyNDcsImV4cCI6MTczNzY5NTY0N30.VGW0asUH0CADMUqZhHW_YPuVVSWfKR6PJIuhJLRvYxI';
 
 /*
 final String uuid = "6e35f08d-8285-45e3-ae32-a0e9efe5407d";

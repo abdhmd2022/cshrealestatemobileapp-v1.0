1f5069721b11871bfd5c4eea886f0864b8a63f5d
apple <1234>
2025-04-18 18:09:32 +0400

    "Commit for real estate mobile app v1.0"

--- lib/MaintenanceTicketReport.dart
+++ lib/MaintenanceTicketReport.dart
@@ -255,7 +255,7 @@ class _MaintenanceTicketReportState extends State<MaintenanceTicketReport> with 
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        "Comments History",
+                        "Comments",
                         style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w600),
                       ),
                       IconButton(

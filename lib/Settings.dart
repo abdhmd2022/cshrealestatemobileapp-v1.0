6873c49e3ba0da78715210b7cd12a636649f1cd4
Muhammad Saadan <saadan@ca-eim.com>
2025-07-02 16:59:20 +0400

    "Commit for real estate mobile app v1.0"

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'AmenitiesReport.dart';
 import 'LeadStatusReport.dart';
 import 'LeadTypeReport.dart';

535c0ec6b69047c9023d7aa696c33735f5d1b792
Muhammad Saadan <saadan@ca-eim.com>
2025-07-18 11:48:19 +0400

    "Commit for real estate mobile app v1.0"

--- lib/ActivitySourceReport.dart
+++ lib/ActivitySourceReport.dart
@@ -219,7 +219,6 @@ class _ActivitySourceReportState extends State<ActivitySourceReport>
                   Navigator.pop(context);
 
                 }
-
               },
               style: ElevatedButton.styleFrom(backgroundColor: appbar_color),
               child: Text("Submit",style: GoogleFonts.poppins(color: Colors.white)),
